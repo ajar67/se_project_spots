7da87abe20d0f4263aa776dda1dda68ec0808ab6
Anthony Ranieri <anthonyjaranieri@gmail.com>
2024-02-29 12:09:23 -0500

    added modal and its css properties and added icon image

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -24,3 +24,16 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
 ];
+//have to upload the icons for the close button
+const editButton = document.querySelector(".profile__edit-button");
+const profileModal = document.getElementById("profile-popup");
+const closeModal = profileModal.querySelector(".modal__button-reset");
+
+editButton.addEventListener("click", () => {
+  console.log("it is getting here");
+  profileModal.classList.add("modal_opened");
+});
+closeModal.addEventListener("click", () => {
+  console.log("it is getting here");
+  profileModal.classList.remove("modal_opened");
+});
