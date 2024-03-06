de804df3b7db1c1d8ccb7c12c9c77f99a7acfa4d
Anthony Ranieri <anthonyjaranieri@gmail.com>
2024-03-06 12:20:26 -0500

    added functionality to new post button & made functions for all reset buttons and for initializing all modals

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -24,38 +24,73 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
 ];
-const editButton = document.querySelector(".profile__edit-button");
-const profileModal = document.getElementById("profile-popup");
-const closeModal = profileModal.querySelector(".modal__button-reset");
-const nameInput = profileModal.querySelector("#name");
-const descriptionInput = profileModal.querySelector("#description");
+
+//CONSTANTS
+
+function initializeModals(modalID) {
+  const modal = document.getElementById(modalID);
+  const formSubmit = modal.querySelector(".modal__form");
+  if (modalID === "profile-popup") {
+    const descriptionInput = modal.querySelector("#description");
+    const nameInput = modal.querySelector("#name");
+    return { modal, descriptionInput, nameInput, formSubmit };
+  }
+  if (modalID === "card-popup") {
+    const linkInput = modal.querySelector("#link");
+    const captionInput = modal.querySelector("#caption");
+    return { modal, linkInput, captionInput, formSubmit };
+  }
+}
+const profileModal = initializeModals("profile-popup");
+const cardModal = initializeModals("card-popup");
+
+//this is all query selectors on the document itself
 const profileTitle = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
-const formSubmit = profileModal.querySelector(".modal__form");
-const template = document.getElementById("template").content;
+const editButton = document.querySelector(".profile__edit-button");
+const addButton = document.querySelector(".profile__add-button");
 const cardsList = document.querySelector(".cards__list");
+const template = document.getElementById("template").content;
 
-function handleOpenModal() {
-  profileModal.classList.add("modal_opened");
-  nameInput.value = profileTitle.textContent;
-  descriptionInput.value = profileDescription.textContent;
+const resetButtons = document.querySelectorAll(".modal__button-reset");
+
+//FUNCTIONS
+
+function handleOpenModal(modal) {
+  modal.classList.add("modal_opened");
 }
 
-function handleCloseModal() {
-  profileModal.classList.remove("modal_opened");
+function handleCloseModal(modal) {
+  modal.classList.remove("modal_opened");
 }
 
-editButton.addEventListener("click", handleOpenModal);
-closeModal.addEventListener("click", handleCloseModal);
+function handleOpenProfileModal() {
+  handleOpenModal(profileModal.modal);
+  profileModal.nameInput.value = profileTitle.textContent;
+  profileModal.descriptionInput.value = profileDescription.textContent;
+}
+
+//EVENT LISTENERS
+
+editButton.addEventListener("click", () =>
+  handleOpenProfileModal(profileModal.modal)
+);
+addButton.addEventListener("click", () => handleOpenModal(cardModal.modal));
+
+resetButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    handleCloseModal(profileModal.modal);
+    handleCloseModal(cardModal.modal);
+  });
+});
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  profileTitle.textContent = nameInput.value;
-  profileDescription.textContent = descriptionInput.value;
-  handleCloseModal();
+  profileTitle.textContent = profileModal.nameInput.value;
+  profileDescription.textContent = profileModal.descriptionInput.value;
+  handleCloseModal(profileModal.modal);
 }
-
-formSubmit.addEventListener("submit", handleProfileFormSubmit);
+profileModal.formSubmit.addEventListener("submit", handleProfileFormSubmit);
 
 function getCardElement(data) {
   const cardElement = template.querySelector(".card").cloneNode(true);
@@ -66,6 +101,7 @@ function getCardElement(data) {
   cardName.textContent = data.name;
   return cardElement;
 }
+
 initialCards.forEach((card) => {
   const newCard = getCardElement(card);
   cardsList.appendChild(newCard);
